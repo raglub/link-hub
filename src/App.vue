68c7c094b0866ccc3282dfa2596489246fba6eec
Michał Szyma <1413586+raglub@users.noreply.github.com>
2020-12-07 23:44:14 +0100

    Downgrade the Vue from 3 to 2 (bootstrap-vue is not compatible with Vue 3)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
+  <div id="app">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {
+  }
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +23,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
     <table style="back">
-      <tr v-for="category in categories" :key="category">
+      <tr v-for="category in categories" :key="category.name">
         <td>{{ category }}</td>
       </tr>
     </table>
@@ -9,29 +10,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Component, Prop, Vue, Model } from 'vue-property-decorator'
 const { ipcRenderer } = window.require('electron')
 
-export default defineComponent({
-  name: 'Home',
-  data: () => {
-    return {
-      categories: []
-    }
-  },
+@Component({
   components: {
-  },
+  }
+})
+export default class Home extends Vue {
+  private categories = [];
+
   async mounted () {
     const data = await ipcRenderer.invoke('get-data')
     this.$data.categories = data.categories
   }
-})
-</script>
-
-<style scoped lang="scss">
-
-table {
-  width: 100%;
 }
-</style>
+</script>
