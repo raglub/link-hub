de163bb449074f5ed9c4b97fe01c73bd757c3b9b
Michał Szyma <1413586+raglub@users.noreply.github.com>
2020-12-11 00:10:43 +0100

    Define configuration for win build

---
+++ vue.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                appId: "dev.szyma.link-hub",
+                productName: 'Link Hub',
+                copyright: "Copyright © 2020 ${author}",
+                publish: {
+                    provider: "github",
+                    owner: "raglub",
+                    repo: "link-hub"
+                },
+                nsis: {
+                    oneClick: false,
+                    artifactName: "link-hub-${version}-${os}64-setup.${ext}",
+                    perMachine: false,
+                    createStartMenuShortcut: true,
+                    runAfterFinish: true
+                },
+                linux: {
+                    icon: 'assets/icons/linux/icon.icns',
+                    target: [
+                        {
+                          target: "deb",
+                          arch: [
+                            "x64"
+                          ]
+                        }
+                    ],
+                    category: "Utility",
+                    packageCategory: "Utility"
+                }
+            }
+        }
+    }
+}
