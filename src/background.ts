ed2cfa807c151ee670a6c47c53539af73c8a4898
Michał Szyma <1413586+raglub@users.noreply.github.com>
2021-01-09 23:47:51 +0100

    Improve the app security by setting nodeIntegration parameter on false

--- src/background.ts
+++ src/background.ts
@@ -4,6 +4,7 @@ import { app, protocol, BrowserWindow, ipcMain } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 import Ipc from './utils/ipc'
+const path = require("path");
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -19,9 +20,12 @@ async function createWindow() {
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: true
       //(process.env
       //    .ELECTRON_NODE_INTEGRATION as unknown) as boolean
+      nodeIntegration: false,
+      // contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false,
+      preload: path.join(__dirname, "/../src/preload.js")
     }
   })
 
--- src/utils/typed-ipc-renderer.ts
+++ src/utils/typed-ipc-renderer.ts
@@ -1,6 +1,5 @@
 import { TypedIpcRenderer } from 'electron-typed-ipc'
 import { IpcEvents } from './ipc-events'
 import { IpcCommands } from './ipc-commands'
-const { ipcRenderer } = window.require('electron')
 
-export default ipcRenderer as TypedIpcRenderer<IpcEvents, IpcCommands>
+export default window.ipcRenderer as TypedIpcRenderer<IpcEvents, IpcCommands>
