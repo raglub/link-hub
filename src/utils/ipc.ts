e6fd2808b6394f1e10daf6be702dba0d9691d14a
Michał Szyma <1413586+raglub@users.noreply.github.com>
2021-01-14 01:53:12 +0100

    Auto register channels for IpcMain.handle

--- src/utils/ipc.ts
+++ src/utils/ipc.ts
@@ -22,7 +22,100 @@ const path = require('path');
 const packageExtract = require('../../package.json') as PackageExctract
 
 const typedIpcMain = ipcMain as TypedIpcMain<IpcEvents, IpcCommands>;
+// ddd.example1()
 
+class IpcApi implements IpcCommands {
+
+	async fetchData () {
+		return Ipc.data
+	}
+
+	async fetchPackage () {
+		return packageExtract
+	}
+
+	async fetchSettings () {
+		const result = new SettingsModel() 
+		const dataPath = await settings.get('dataPath');
+		if (dataPath) {
+			result.dataPath = dataPath.toString()
+		}
+		return result
+	}
+
+	async updateSettings (newSettings: SettingsModel) {
+		await settings.set('dataPath', newSettings.dataPath)
+		// await this.loadData()
+		return newSettings
+	}
+
+	async createLink (categoryId: string, link: Link) {
+		const category = Ipc.data.categories.find(category => category.id === categoryId)
+		if (category) {
+			link.id = uuidv4()
+			category.links.push(link)
+		}
+		fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
+		return Ipc.data
+	}
+
+	async deleteCategory (categoryId: string) {
+		Ipc.data.categories = Ipc.data.categories.filter(category => category.id !== categoryId)
+		fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
+	}
+
+	async deleteLink (linkId: string) {
+		const response = new IpcResponse<boolean>()
+		response.response = false
+		try {
+			const category = Ipc.data.categories.find(category => category.links.find(link => link.id === linkId))
+			if (category) {
+				category.links = category.links.filter(link => link.id !== linkId)
+				fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
+				response.response = true
+			}
+		} catch (err) {
+			response.error = err	
+		}
+		return response
+	}
+
+	async createCategory (categoryCreate: CategoryCreate) {
+		const category = new Category()
+		category.name = categoryCreate.name
+		category.id = uuidv4()
+		Ipc.data.categories.push(category)
+		fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
+		return Ipc.data
+	}
+
+	async editLink (link: LinkEdit) {
+		const linkId = link.id
+		const category = Ipc.data.categories.find(category => category.links.find(link => link.id === linkId))
+		if (category) {
+			const categoryLink = category.links.find(link => link.id === linkId)
+			if (categoryLink) {
+				categoryLink.name = link.name
+				categoryLink.url = link.url
+			}
+		}
+		fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
+		return Ipc.data
+	}
+
+	async openUrl (url: string) {
+		shell.openExternal(url)
+	}
+
+	async editCategory (categoryEdit: CategoryEdit) {
+		const category = Ipc.data.categories.find(category => category.id === categoryEdit.id)
+		if (category) {
+			category.name = categoryEdit.name
+		}
+		fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
+		return Ipc.data
+	}
+}
 export default class Ipc {
 	static dataPath = ''
 
@@ -59,95 +152,12 @@ export default class Ipc {
 			});
 		})
 		*/
-
-		typedIpcMain.handle(IpcChannel.fetchData, async (event) => {
-			return Ipc.data
-		})
-
-		typedIpcMain.handle(IpcChannel.fetchPackage, async (event) => {
-			return packageExtract
-		})
-
-		typedIpcMain.handle(IpcChannel.fetchSettings, async (event) => {
-			const result = new SettingsModel() 
-			const dataPath = await settings.get('dataPath');
-			if (dataPath) {
-				result.dataPath = dataPath.toString()
-			}
-			return result
-		})
-
-		typedIpcMain.handle(IpcChannel.updateSettings, async (event, newSettings: SettingsModel) => {
-			await settings.set('dataPath', newSettings.dataPath)
-  		await this.loadData()
-			return newSettings
-		})
-
-		typedIpcMain.handle(IpcChannel.createLink, async (event: any, categoryId: string, link: Link) => {
-			const category = Ipc.data.categories.find(category => category.id === categoryId)
-			if (category) {
-				link.id = uuidv4()
-				category.links.push(link)
-			}
-			fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
-			return Ipc.data
-		})
-
-		typedIpcMain.handle(IpcChannel.deleteCategory, async (event: any, categoryId: string) => {
-			Ipc.data.categories = Ipc.data.categories.filter(category => category.id !== categoryId)
-			fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
-		})
-
-		typedIpcMain.handle(IpcChannel.deleteLink, async (event: any, linkId: string) => {
-			const response = new IpcResponse<boolean>()
-			response.response = false
-			try {
-				const category = Ipc.data.categories.find(category => category.links.find(link => link.id === linkId))
-				if (category) {
-					category.links = category.links.filter(link => link.id !== linkId)
-					fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
-					response.response = true
-				}
-			} catch (err) {
-				response.error = err	
-			}
-			return response
-		})
-
-		ipcMain.handle(IpcChannel.createCategory, async (event, categoryCreate: CategoryCreate) => {
-			const category = new Category()
-			category.name = categoryCreate.name
-			category.id = uuidv4()
-			Ipc.data.categories.push(category)
-			fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
-			return Ipc.data
-		})
-
-		ipcMain.handle(IpcChannel.editLink, async (event, link: LinkEdit) => {
-			const linkId = link.id
-			const category = Ipc.data.categories.find(category => category.links.find(link => link.id === linkId))
-			if (category) {
-				const categoryLink = category.links.find(link => link.id === linkId)
-				if (categoryLink) {
-					categoryLink.name = link.name
-					categoryLink.url = link.url
-				}
-			}
-			fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
-			return Ipc.data
-		})
-
-		ipcMain.handle(IpcChannel.openUrl, async (event, url: string) => {
-			shell.openExternal(url)
-		})
-
-		ipcMain.handle(IpcChannel.editCategory, async (event, categoryEdit: CategoryEdit) => {
-			const category = Ipc.data.categories.find(category => category.id === categoryEdit.id)
-			if (category) {
-				category.name = categoryEdit.name
-			}
-			fs.writeFileSync(Ipc.dataPath, JSON.stringify(Ipc.data, null, 2));
-			return Ipc.data
-		})
+		const api = new IpcApi()
+		for (var apiKey in api) {
+			const method = (api as any)[apiKey] 
+			ipcMain.handle(apiKey, (event: any, ...args: any): any => {
+				return method(...args)
+			})
+		}
 	}
 }
